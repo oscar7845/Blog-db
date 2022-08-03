0a703ec10a1d30201b2db0eb1ef4513871c0fb4f
Oscar Callo von Platen <98499720+oscar7845@users.noreply.github.com>
2022-08-03 12:53:31 +0200

    Add files via upload

---
+++ blog/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+define('ROOT_URL', 'http://localhost/blog/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'oscar');
+define('DB_PASS', 'Nadal2002');
+define('DB_NAME', 'blog');
